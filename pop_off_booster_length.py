38abf5212e56a74dc32a7cb83e98c4e322931996
jakewood0 <jlwoodside06@gmail.com>
2025-04-24 13:44:46 -0400

    Update pop_off_booster_length.py

--- pop_off_booster_length.py
+++ pop_off_booster_length.py
@@ -20,7 +20,7 @@ def compute_L1(burn_time, length):
     
     R = math.exp((burn_time) / (2 * constants.Isp)) # Mass Ratio Defined by Burn Time
     
-    A = (np.pi / 4) * ((constants.d_total)**2 - (constants.d_prop**2)) * constants.rho_wall
+    A = (np.pi / 4) * ((constants.d_total**2) - (constants.d_prop**2)) * constants.rho_wall
     B = (np.pi / 4) * (constants.d_total ** 2) * (constants.L_bulkhead) * (constants.rho_bulkhead)
     C = (np.pi / 4) * (constants.d_prop**2) * constants.rho_wall
     D = M + (C * L) + (A * L) + (3 * B)
