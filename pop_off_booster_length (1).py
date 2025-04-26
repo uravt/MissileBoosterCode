dbdbc477e601f70948220b9b3e73a5d224a9d6eb
jakewood0 <jlwoodside06@gmail.com>
2025-04-25 20:11:37 -0400

    Update pop_off_booster_length (1).py

--- pop_off_booster_length (1).py
+++ pop_off_booster_length (1).py
@@ -4,7 +4,7 @@ import constants
 
 def compute_L1(burn_time, length_total):
     M = constants.m_payload
-    L_r = length_total - (3 * constants.L_bulkhead) # Reduced length
+    Lr = length_total - (3 * constants.L_bulkhead) # Reduced length
     
     R = math.exp((burn_time) / (2 * constants.Isp)) # Mass Ratio Defined by Burn Time
     
@@ -12,7 +12,7 @@ def compute_L1(burn_time, length_total):
     B = (np.pi / 4) * (constants.d_total ** 2) * (constants.L_bulkhead) * (constants.rho_bulkhead)
     C = (np.pi / 4) * (constants.d_prop**2) * constants.rho_prop
     D = A + C
-    E = M + (3 * B) + (D * L_r)
+    E = M + (3 * B) + (D * Lr)
     F = E * (R - 1)
     G = C * R
 
