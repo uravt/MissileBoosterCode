b31bae43c3f6688988c27360e27d49da9125513f
jakewood0 <jlwoodside06@gmail.com>
2025-04-29 13:14:19 -0400

    Update pop_off_booster_length.py

--- pop_off_booster_length.py
+++ pop_off_booster_length.py
@@ -1,40 +1,19 @@
-import math
 import numpy as np
 import constants
 
-def solve_quadratic(a, b, c, contraint):
-    # Calculate the discriminant
-    discriminant = math.sqrt((b ** 2) - 4 * a * c)
-    # Calculate two solutions
-    x1 = (-b + discriminant) / (2 * a)
-    x2 = (-b - discriminant) / (2 * a)
-
-    if ((x1 < contraint) and (x1 > 0)):
-        return x1
-    else:
-        return x2
-        
-def compute_L1(burn_time, length):
+def compute_L1(burn_time, length_total):
     M = constants.m_payload
-    L = length
+    L_r = length_total - (3 * constants.L_bulkhead) # Reduced length
     
     R = math.exp((burn_time) / (2 * constants.Isp)) # Mass Ratio Defined by Burn Time
     
     A = (np.pi / 4) * ((constants.d_total**2) - (constants.d_prop**2)) * constants.rho_wall
     B = (np.pi / 4) * (constants.d_total ** 2) * (constants.L_bulkhead) * (constants.rho_bulkhead)
     C = (np.pi / 4) * (constants.d_prop**2) * constants.rho_prop
-    D = M + (C * L) + (A * L) + (3 * B)
-    E = M + (C * L) + (A * L) + (2 * B)
-    F = A + C
-    G = (B ** 2) + (D * E)
-    H = (A * B) + (B * F) - (C * E) - (D * F)
-    I = (A * F) + (C * F)
-    J = (B * D)
-    K = (A * D) - (B * C)
-    N = (A * C)
-    O = (I + (R * N))
-    Q = H - (K * R)
-    S = (G - (R * J))
+    D = A + C
+    E = M + (3 * B) + (D * L_r)
+    F = E * (1 - R)
 
-    L1 = solve_quadratic(O, Q, S, length)
+    L1 = F / C
+
     return L1
